987eeea56c7658fa4a560bf9cb79d57a64ec54cb
sungguenja <sungguenja@gmail.com>
2022-03-16 00:46:24 +0900

    chore-frontend: react router 설치

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// routes
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/expenses" element={<Expenses />} />
+      <Route path="/invoices" element={<Invoices />} />
+      {/* 
+        outlet을 이용해 상위 컴포넌트 내에서 하위 컴포넌트 모습만 보이고 싶을때 
+      <Route path="/test/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Route> 
+      */}
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
