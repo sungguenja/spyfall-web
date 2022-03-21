c8e5c6e0b2d7c3bf0e7b551e082779a7d37856f6
sungguenja <sungguenja@gmail.com>
2022-03-21 23:04:28 +0900

    init-backend: kotlin으로 백엔드 빌드

---
+++ backend/src/main/kotlin/com/spyfall/kspyfallweb/KspyfallWebApplication.kt
@@ -0,0 +1,11 @@
+package com.spyfall.kspyfallweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KspyfallWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<KspyfallWebApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/spyfall/kspyfallweb/KspyfallWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.spyfall.kspyfallweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KspyfallWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
